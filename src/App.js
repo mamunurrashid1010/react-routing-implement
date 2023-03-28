ed44c852cd23a7e1350c8fde5c68264a6f4d8819
mamunurrashid1010 <mamunurrashid1010@gmail.com>
2023-03-28 11:50:49 +0600

    route add for error page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from "./pages/Home";
 import Blog from "./pages/Blog";
 import Service from "./pages/Service";
 import Contact from "./pages/Contact";
+import Error from "./pages/Error";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
               <Route path="/blog" element={<Blog/>} />
               <Route path="/service" element={<Service/>} />
               <Route path="/contact" element={<Contact/>} />
+              <Route path="*" element={<Error/>} />
           </Routes>
       </BrowserRouter>
     </div>
