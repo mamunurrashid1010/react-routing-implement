b462a5668d05d155205758e3459e841937e7bcdc
mamunurrashid1010 <mamunurrashid1010@gmail.com>
2023-03-28 10:35:30 +0600

    app routes add

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Home from "./pages/Home";
+import Blog from "./pages/Blog";
+import Service from "./pages/Service";
+import Contact from "./pages/Contact";
 
 function App() {
   return (
     <div className="App">
-      <h1>My react app</h1>
+      <BrowserRouter>
+          <Routes>
+              <Route path="/" element={<Home/>} />
+              <Route path="/blog" element={<Blog/>} />
+              <Route path="/service" element={<Service/>} />
+              <Route path="/contact" element={<Contact/>} />
+          </Routes>
+      </BrowserRouter>
     </div>
   );
 }
