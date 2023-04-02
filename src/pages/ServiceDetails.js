067b853a2c6ae28288963982e0705b5a6d987df4
mamunurrashid1010 <mamunurrashid1010@gmail.com>
2023-04-02 12:04:44 +0600

    query parameter example add

--- src/pages/ServiceDetails.js
+++ src/pages/ServiceDetails.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import {useParams} from "react-router-dom";
+import {useNavigate, useParams, useSearchParams} from "react-router-dom";
 
 const ServiceDetails = () => {
     const {title} = useParams();
+    const [mySearchParam] = useSearchParams()
     return (
         <div>
-            <h5>{title}</h5>
+            <h5>Title : {title}</h5>
+            <h6>Name  : {mySearchParam.get('name')}</h6>
+            <h6>Age   : {mySearchParam.get('age')}</h6>
         </div>
     );
 };
